14fce575eba2dc4505d112eda42587faa1554b09
robochi_bot <robochi.work@gmail.com>
2025-12-10 17:01:32 +0300

    Удалены из отслеживания ранее закоммиченные игнорируемые файлы

--- telegram/static/js/menu.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    const menu = document.getElementById("menu");
-    if (menu.style.display === "none" || menu.style.display === "") {
-        menu.style.display = "flex";
-    } else {
-        menu.style.display = "none";
-    }
-}
