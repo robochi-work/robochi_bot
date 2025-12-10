e7d1fcfb53475a473750913ee954d60ba2cf104a
robochi_bot <robochi.work@gmail.com>
2025-12-10 17:03:33 +0300

    Добавлены изменения с сервера: удалены артефакты, обновлены исходники

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ~/robochi_bot
 git pull origin main
-source .venv/bin/activate
+source venv/bin/activate
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --noinput
