0f679c8438955c5b9422d8a8432b085cfe198b48
robochi_bot <robochi.work@gmail.com>
2026-01-06 16:23:56 +0300

    Update work profile steps and related templates

--- work/urls.py
+++ work/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 
-from work.views.work_profile import questionnaire_redirect, ProfileWizard, work_profile_detail
+from work.views.work_profile import (
+    questionnaire_redirect,
+    ProfileWizard,
+    work_profile_detail,
+    agreement_view,
+)
 
 app_name = 'work'
+
 urlpatterns = [
+    path('agreement/', agreement_view, name='agreement'),
+    path('profile/', work_profile_detail, name='work_profile_detail'),
+
     path('anketa/', questionnaire_redirect, name='anketa'),
     path('anketa/<step>/', ProfileWizard.as_view(), name='anketa_step'),
-    path('profile/', work_profile_detail, name='work_profile_detail'),
 ]
