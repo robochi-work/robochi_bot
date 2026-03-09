4e1b80646a14f1f2ade1e4f986af8569dbb55d1d
robochi_bot <robochi.work@gmail.com>
2026-03-09 13:16:30 +0200

    fix: deploy.sh использует ветку develop и venv python

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd ~/robochi_bot
-git pull origin main
+git pull origin develop
 source venv/bin/activate
-pip install -r requirements.txt
-python manage.py migrate
-python manage.py collectstatic --noinput
-python manage.py compilemessages
+./venv/bin/pip install -r requirements.txt
+./venv/bin/python manage.py migrate
+./venv/bin/python manage.py collectstatic --noinput
+./venv/bin/python manage.py compilemessages
 systemctl restart gunicorn
 systemctl restart celery-worker
 systemctl restart celery-beat
